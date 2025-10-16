36c544031eacce2e6b4cb058d13b1aee54eca054
Ishimwechille <155310525+Ishimwechille@users.noreply.github.com>
2025-10-16 10:34:39 +0300

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -5,7 +5,6 @@ A simple Python script to create a MySQL database named 'alx_book_store'.
 """
 
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     try:
@@ -23,7 +22,7 @@ def create_database():
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
 
     finally:
